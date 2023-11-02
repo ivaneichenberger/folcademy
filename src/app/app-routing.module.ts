bd535c69a8014d50a93ebfbff8677290538aa9cd
John Doe <145499267+ivaneichenberger@users.noreply.github.com>
2023-11-02 14:55:46 -0300

    desafio 6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/routes/home/home.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
   },
-  
+  {
+    path: 'login',
+    component: LoginComponent
+  },
   {
     path: '',
     redirectTo: 'home',
